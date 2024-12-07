363204ca96385741c0b3190ece66ccf2026e15f1
rodrigoaustincascao <rodrigoaustincascao@gmail.com>
2024-12-07 11:38:30 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from "./tasks/tasks.component";
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'essentials';
+  users = DUMMY_USERS;
+  selectedUserId?: string;
+
+  get selectedUser(){
+    return this.users.find((user) => user.id == this.selectedUserId)!;
+  }
+
+  onSelectUser(id:string){
+    this.selectedUserId = id;
+  }
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, Output} from '@angular/core';
+
+interface User {
+  id:  string;
+  avatar: string;
+  name: string;
+}
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.scss'
+})
+export class UserComponent {
+  @Input({required: true}) user!: User
+  @Output()select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser(){
+    this.select.emit(this.user.id);
+  }
+}
